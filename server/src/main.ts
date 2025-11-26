5aa543a229f04f73756b08b8dee7a09b58f27c1b
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-11-26 16:50:05 +0800

    chore: integrate socket.io to server and client

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,27 @@ import { Server } from 'socket.io';
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  /* options */
+  cors: {
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5173'
+        : undefined,
+  },
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
 
 io.on('connection', (socket) => {
-  // ...
+  console.log('user connected: ', socket.id);
+  socket.on('foo', (message, callback) => {
+    io.emit('foo', message);
+    callback('got it');
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 httpServer.listen(3000, () => {
