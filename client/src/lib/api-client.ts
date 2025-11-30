c5a2183c0d7041b7b26c2d7238d8963bbf6195d4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-11-30 13:44:32 +0800

    feat: create `fetchMessage` data loader

--- client/src/lib/api-client.ts
+++ client/src/lib/api-client.ts
@@ -16,3 +16,18 @@ export const api = ky.create({
     ],
   },
 });
+
+export async function fetchMessages() {
+  return [
+    'Congratulations to all the Lions Awards winners! Thanks for your incredible hard work this year!',
+    'Shout-out to the night shift team! You keep everything running smoothly — we appreciate you!',
+    'Proud to be part of this amazing team! Let’s keep pushing boundaries and making magic happen!',
+    'Huge thanks to everyone who supported me this year. Couldn’t have done it without you!',
+    'Let’s celebrate the wins, big and small. You’re all amazing!',
+    'Cheers to teamwork, growth, and success. Let’s keep the momentum going!',
+    'So grateful for the friendships and support this year. Happy Lions Awards!',
+    'Keep shining, team! Your commitment makes a real difference every day.',
+    'To my squad: you rock! Here’s to smashing more goals together next year!',
+    'Big applause for the unsung heroes behind the scenes. We see you! Thank you!',
+  ];
+}
--- client/src/routes/board.tsx
+++ client/src/routes/board.tsx
@@ -1,14 +1,20 @@
 import { ChatBubble } from '@/components/chat-bubble';
+import { fetchMessages } from '@/lib/api-client';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/board')({
   component: RouteComponent,
+  loader: () => fetchMessages(),
 });
 
 function RouteComponent() {
+  const messages = Route.useLoaderData();
+
   return (
     <div className="p-8">
-      <ChatBubble message="That's awesome. I think our users will really appreciate the improvements." />
+      {messages.map((message) => (
+        <ChatBubble message={message} />
+      ))}
     </div>
   );
 }
